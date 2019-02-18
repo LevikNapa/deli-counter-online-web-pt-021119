902d2ece44da550fa090eae9b1c058d6281d9d58
Levik Naparstek <levik010@gmail.com>
2019-02-18 22:47:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,11 @@
-# Write your code here.
-katz_deli = []
-
-def line (katz_deli)
-  katz_deli >> "steve", "john", "mary"
-  
-  katz_deli.each.index(1) do |n|
-    "The line is currently: #{katz_deli}"
+def line(x)
+  line_array = []
+  if x.length == 0
+    puts "The line is currently empty."
+  else
+    x.each.with_index(1) do |name, index|
+      line_array.push("#{index}. #{name}")
+    end
+    puts "The line is currently: #{line_array.join(" ")}"
   end
-  
 end
