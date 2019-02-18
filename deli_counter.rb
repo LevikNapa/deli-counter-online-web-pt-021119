5b78c482a9d12eae1c364bfcd4111e592559a3a4
Levik Naparstek <levik010@gmail.com>
2019-02-18 22:53:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,6 +10,18 @@ def line(x)
   end
 end
 
-def take_a_number(line, "name")
-  line = []
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(line)
+  
+  
+    
+    
+    
+    
+    
+    
   
