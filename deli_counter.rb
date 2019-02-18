e518b548e8db41cdf2e1f35165228e5f061abf5f
Levik Naparstek <levik010@gmail.com>
2019-02-18 22:41:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@
 katz_deli = []
 
 def line (katz_deli)
-  katz_deli.push ("steve", "john", "mary")
+  katz_deli >> "steve", "john", "mary"
   
   katz_deli.each.index(1) do |n|
     "The line is currently: #{katz_deli}"
